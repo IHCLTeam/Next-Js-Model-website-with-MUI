573a63d4cb7bec012b3d2b6d20d8f775d14ca62a
unleash141 <ihclsanityusers@gmail.com>
2022-04-12 18:42:47 +0530

    initial commit

--- components/Login/Login.tsx
+++ components/Login/Login.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Login() {
   return (
     <Grid>
-        Login Here
+        Login
     </Grid>
   )
 }
--- components/Regester/Regester.tsx
+++ components/Regester/Regester.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function Regester() {
   return (
     <Grid>
-        Regester Here
+        Regester
     </Grid>
   )
 }
