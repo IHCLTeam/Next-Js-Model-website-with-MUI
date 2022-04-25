b6f8cf56aa6fca22be7fd3216a626dcaf8dfff2e
unleash141 <ihclsanityusers@gmail.com>
2022-04-25 11:48:49 +0530

    Update on footer icons

--- pages/components/Layout/Footer.tsx
+++ pages/components/Layout/Footer.tsx
@@ -3,6 +3,7 @@ import imageLoader from "../../ImageLoader";
 import Image from "next/image";
 import * as React from "react";
 import Grid from "@mui/material/Grid";
+import { autoAction } from "mobx/dist/internal";
 
 const styles = {
   paperContainer: {
@@ -37,15 +38,16 @@ function Footer() {
             />
           </Item>
         </Grid>
-        <Grid container spacing={1} item xs={4}>
+        <Grid container spacing={0} item xs={4}>
           <Item
             sx={{
               boxShadow: "none",
               textAlign: "center",
               color: "white",
+              marginLeft : '18%'
             }}
           >
-            <svg
+            {/* <svg
               width={"5%"}
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 448 512"
@@ -87,7 +89,43 @@ function Footer() {
                 fill="currentColor"
                 d="M100.28 448H7.4V148.9h92.88zM53.79 108.1C24.09 108.1 0 83.5 0 53.8a53.79 53.79 0 0 1 107.58 0c0 29.7-24.1 54.3-53.79 54.3zM447.9 448h-92.68V302.4c0-34.7-.7-79.2-48.29-79.2-48.29 0-55.69 37.7-55.69 76.7V448h-92.78V148.9h89.08v40.8h1.3c12.4-23.5 42.69-48.3 87.88-48.3 94 0 111.28 61.9 111.28 142.3V448z"
               ></path>
-            </svg>
+            </svg> */}
+           
+            <Image 
+            loader={imageLoader}
+            src = 'https://www.tata.com/content/dam/tata/images/footer/FB.svg'
+            alt="image not found"
+            width = {'65%'}
+            height = '32%'
+            />
+            <Image 
+            loader={imageLoader}
+            src = 'https://www.tata.com/content/dam/tata/images/footer/Linkedin.svg'
+            alt="image not found"
+            width = {'65%'}
+            height = '32%'
+            />
+            <Image 
+            loader={imageLoader}
+            src = 'https://www.tata.com/content/dam/tata/images/footer/twitter_icon.svg'
+            alt="image not found"
+            width = {'65%'}
+            height = '30%'
+            />
+            <Image 
+            loader={imageLoader}
+            src = 'https://www.tata.com/content/dam/tata/images/footer/video.svg'
+            alt="image not found"
+            width = {'62%'}
+            height = '30%'
+            />
+            <Image 
+            loader={imageLoader}
+            src = 'https://www.tata.com/content/dam/tata/images/footer/Instagram.svg'
+            alt="image not found"
+            width = {'65%'}
+            height = '30%'
+            />
           </Item>
         </Grid>
 
